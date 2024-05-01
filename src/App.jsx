cab231ba24aae3fb42178c0679d469267a50b26b
Ade <ade.nahuelpan.s@gmail.com>
2024-04-30 20:17:27 -0400

    added on construction page

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import wgLogo from './assets/wgLogo'
+import wgLogo from './assets/wg-logo.png'
+import background from './assets/newFondo.png';
+import { Grid, Box } from '@mui/material';
+import construction from './pages/Construction'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://www.twitch.tv/WhispCL" target="_blank">
-          <img src={wgLogo} className="logo react" alt="WG logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Grid container sx={{backgroundImage: `url(${background})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    width: '100vw',
+    height: '100vh',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    }}>
+      
+    </Grid>
+  );
 }
 
 export default App
---
+++ src/pages/Construction.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import background from '../assets/newFondo.png';
+import wgLogo from '../assets/wg-logo.png';
+
+const OnConstruction = () => {
+    return (
+        <img src={background} alt="" />
+    )
+};
+
+export default OnConstruction;
