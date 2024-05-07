7f8b503d899adf5b92ccab494f5f6c04c29806df
Ade <ade.nahuelpan.s@gmail.com>
2024-05-07 15:02:46 -0400

    added react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import wgLogo from './assets/wg-logo.png'
 import background from './assets/newFondo.png';
-import { Grid, Box } from '@mui/material';
+import { Grid, Box, Typography, Button } from '@mui/material';
 import construction from './pages/Construction'
+import { useNavigate } from 'react-router-dom';
 import './App.css'
 
 function App() {
-  
+  const navigate = useNavigate();
   return (
-    <Grid container sx={{backgroundImage: `url(${background})`,
+    <Grid container display='flex' sx={{backgroundImage: `url(${background})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     width: '100vw',
@@ -19,7 +20,13 @@ function App() {
     top: 0,
     left: 0,
     }}>
-      
+      <Grid container sx={{display:'flex', alignContent:'center', justifyContent:'center', flexDirection:'column', mt: 6,}} >
+        <Grid item>
+        <img width='256' src={wgLogo} href= 'https://twitter.com/whisp_gaming' /> 
+        </Grid>
+        <Typography variant='h1'>Whisp Gaming</Typography>
+        
+      </Grid>
     </Grid>
   );
 }
